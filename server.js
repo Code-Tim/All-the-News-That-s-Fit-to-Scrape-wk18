143b4db30b0ddd8a28e7f4d1bb8796bd3562bae6
Code-Tim <62041831+Code-Tim@users.noreply.github.com>
2020-08-07 11:57:23 -0400

    scrape successful

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/populatedb", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/populatedb", { useNewUrlParser: true });
 
 
 
@@ -29,4 +29,53 @@ mongoose.connect("mongodb://localhost/populatedb", { useNewUrlParser: true });
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true, useUnifiedTopology: true
+});
+
+//cheerio
+const cheerio = require("cheerio")
+const axios = require("axios")
+var db = require("./models")
+app.get("/scrape", function (req, res) {
+    // First, we grab the body of the html with axios
+    axios.get("https://www.bbc.com/sport").then(function (response) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
+        // console.log(response.data)
+        // Now, we grab every h2 within an article tag, and do the following:
+        $("h3.gs-c-promo-heading__title").each(function (i, element) {
+            console.log($(element).text())
+            console.log($(element).parent().attr("href"))
+
+            var result = {};
+
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(element).text()
+            result.link = $(element).parent().attr("href")
+
+            // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    // If an error occurred, log it
+                    console.log(err);
+                });
+        })
+        res.send("All Done")
+    })
+
+
+
+
+
+
+
+
+})
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+})
