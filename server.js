da42b3664adfa5c8983c0df2e6a4acac3903c563
Code-Tim <62041831+Code-Tim@users.noreply.github.com>
2020-08-07 16:02:12 -0400

    serverJS

--- server.js
+++ server.js
@@ -75,6 +75,7 @@ app.get("/scrape", function (req, res) {
 app.get("/articles", function (req, res) {
     // Grab every document in the Articles collection
     db.Article.find({})
+        .populate("Note")
         .then(function (dbArticle) {
             // If we were able to successfully find Articles, send them back to the client
             res.json(dbArticle);
@@ -90,7 +91,7 @@ app.get("/articles/:id", function (req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({ _id: req.params.id })
         // ..and populate all of the notes associated with it
-        .populate("note")
+        .populate("Note")
         .then(function (dbArticle) {
             // If we were able to successfully find an Article with the given id, send it back to the client
             res.json(dbArticle);
