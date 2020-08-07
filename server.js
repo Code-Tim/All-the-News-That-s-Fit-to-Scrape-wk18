197a2709db52f9e155ffde52ebb2678e9c3423ca
Code-Tim <62041831+Code-Tim@users.noreply.github.com>
2020-08-07 16:20:04 -0400

    changed the models ext on serverJS

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const cheerio = require("cheerio")
 const axios = require("axios")
 var db = require("./models")
 // Require all models
-var db = require("./models");
+// var db = require("./models/Article");
+const Article = require("./models/Article");
+const Note = require("./models/Note.js");
 
 // Initialize Express
 var app = express();
@@ -75,7 +77,7 @@ app.get("/scrape", function (req, res) {
 app.get("/articles", function (req, res) {
     // Grab every document in the Articles collection
     db.Article.find({})
-        .populate("Note")
+        .populate("note")
         .then(function (dbArticle) {
             // If we were able to successfully find Articles, send them back to the client
             res.json(dbArticle);
@@ -91,7 +93,7 @@ app.get("/articles/:id", function (req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({ _id: req.params.id })
         // ..and populate all of the notes associated with it
-        .populate("Note")
+        .populate("note")
         .then(function (dbArticle) {
             // If we were able to successfully find an Article with the given id, send it back to the client
             res.json(dbArticle);
